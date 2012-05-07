16df6f9eeaa93e6fd3ae1a2d27ec54a57cf323bb
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-07 16:19:30 -0300

    Novo projeto para testes
    apenas para testes
    
    Signed-off-by: Paulo de Lima Cavalcanti <paulolimac@gmail.com>

---
+++ MyTestProject/src/com/paulolimac/mytestproject/HelloProject.java
@@ -0,0 +1,13 @@
+/*
+ * Classe de teste, para impressão simples
+ */
+
+package com.paulolimac.mytestproject;
+
+public class HelloProject {
+	
+	public static void main(String[] args) {
+		System.out.println("O projeto deixou um 'olá'!");
+	}
+
+}
