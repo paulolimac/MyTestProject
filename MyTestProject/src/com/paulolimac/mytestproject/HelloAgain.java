a9cdf0b7cb24205b351e7c36e8293542fdf1b8bb
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-07 16:26:05 -0300

    Segunda classe que diz olá
    Signed-off-by: Paulo de Lima Cavalcanti <paulolimac@gmail.com>

---
+++ MyTestProject/src/com/paulolimac/mytestproject/HelloAgain.java
@@ -0,0 +1,13 @@
+/*
+ * Outra classe...que diz 'olá'
+ */
+
+package com.paulolimac.mytestproject;
+
+public class HelloAgain {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá de novo");
+	}
+
+}
