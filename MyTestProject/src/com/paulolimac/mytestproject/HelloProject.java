e87a4701636ea0bd6b0c25d63935505509663240
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-09 10:32:00 -0300

    teste do staged
    Signed-off-by: Paulo de Lima Cavalcanti <paulolimac@gmail.com>

--- MyTestProject/src/com/paulolimac/mytestproject/HelloProject.java
+++ MyTestProject/src/com/paulolimac/mytestproject/HelloProject.java
@@ -8,6 +8,7 @@ public class HelloProject {
 	
 	public static void main(String[] args) {
 		System.out.println("O projeto deixou um 'olá'!");
+		System.out.println("Tentando deixá-lo 'staged'");
 	}
 
 }
