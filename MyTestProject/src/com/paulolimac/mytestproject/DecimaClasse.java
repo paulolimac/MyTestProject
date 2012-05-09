babfa96f7a7291b02893343126133fc2301a7c8d
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-09 13:14:01 -0300

    DecimaClasse criada no working
    Signed-off-by: Paulo de Lima Cavalcanti <paulolimac@gmail.com>

---
+++ MyTestProject/src/com/paulolimac/mytestproject/DecimaClasse.java
@@ -0,0 +1,10 @@
+package com.paulolimac.mytestproject;
+
+public class DecimaClasse {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("DecimaClasse criada no working");
+	}
+
+}
