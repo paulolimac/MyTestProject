88c2d97b4e20194967caa0555265a9eb8ce56be2
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-09 13:08:19 -0300

    Mudança na classe 6 pelo master
    Signed-off-by: Paulo de Lima Cavalcanti <paulolimac@gmail.com>

--- MyTestProject/src/com/paulolimac/mytestproject/SextaClasse.java
+++ MyTestProject/src/com/paulolimac/mytestproject/SextaClasse.java
@@ -4,6 +4,7 @@ public class SextaClasse {
 	
 	public static void main(String[] args) {
 		System.out.println("SextaClasse criada no master");
+		//mudança sem mudar no master, para criar novo commit
 	}
 
 }
