4f2a9bf965e44780b5324a08beb9006969e4974f
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-09 13:02:07 -0300

    QuintaClasse criada pelo working
    Signed-off-by: Paulo de Lima Cavalcanti <paulolimac@gmail.com>

---
+++ MyTestProject/src/com/paulolimac/mytestproject/QuintaClasse.java
@@ -0,0 +1,10 @@
+package com.paulolimac.mytestproject;
+
+public class QuintaClasse {
+	
+	public static void main(String[] args) {
+		System.out.println("quinta classe criada pelo working");
+	}
+	
+
+}
