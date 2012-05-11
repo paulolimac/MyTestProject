e39f6c11a8d23abe9e0eadf64e5ab4fe58c799c1
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-11 18:28:44 -0300

    classe2

--- MyTestProject/src/com/paulolimac/mytestproject/TerceiraClasse.java
+++
@@ -1,9 +0,0 @@
-package com.paulolimac.mytestproject;
-
-public class TerceiraClasse {
-	
-	public static void main(String[] args) {
-		System.out.println("Terceira classe criada no branch working");
-	}
-
-}
